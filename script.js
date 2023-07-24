31b0648117394258500c7cf0dfaa8f310da2b2e0
aditya bakshi <96980869+professor56@users.noreply.github.com>
2023-07-24 17:36:00 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -17,12 +17,12 @@ b.addEventListener('click',function () {
     b.style.border="8px white soild;"
 })
 document.getElementById("btn").addEventListener('click',function () {
-         
+         if(b.value=='google'){
         location.href ='https://www.google.com'
         new Notification('Dotcom',{
         body:'Google'
         })
-
+    }
 		
     
     if(b.value=='facebook'){
