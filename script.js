ee48f9276439f1ca5c720c7e3ce1f275f1d6339c
aditya bakshi <96980869+professor56@users.noreply.github.com>
2023-07-24 17:31:02 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
 var b = document.getElementById('io');
+
+Notification.requestPermission().then(p=>{
+     if(p=='granted'){
+        new Notification('Dotcom',{
+            body:'Notification Allow '
+        })
+        if(p=='denied'){
+            new Notification('Dotcom',{
+                body:'Notification Not  Allow '
+            })
+        }
+     }
+})
+
 b.addEventListener('click',function () {
     b.style.border="8px white soild;"
 })
 document.getElementById("btn").addEventListener('click',function () {
-    if (b.value=='google') {
-        
-			location.href ='https://www.google.com'
+         
+        location.href ='https://www.google.com'
+        new Notification('Dotcom',{
+        body:'Google'
+        })
 
 		
-    }
+    
     if(b.value=='facebook'){
         location.href ='https://www.facebook.com'
+        
+        new Notification('Dotcom',{
+            body:'Facebook  '
+        })
+    }
+    if(b.value=='instragram'){
+        location.href="https://www.instragram.com"
+        new Notification('Dotcom',{
+            body:'Instragram'
+        })
     }
 })
